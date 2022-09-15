0e6e114074c19893d7c400eae9884b01976dda20
Owen Arden <owenarden@gmail.com>
2022-09-14 17:48:18 -0700

    ported 01-haskell to new test framework

--- tests/Common.hs
+++ tests/Common.hs
@@ -1,16 +1,21 @@
 
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE RecordWildCards #-}
 
 module Common where
 
 import Data.IORef
+import Data.List
+import Data.Maybe
 import Test.Tasty
+import Test.Tasty.ExpectedFailure
 import Test.Tasty.HUnit
 import Test.Tasty.Runners
 import System.Exit
 import Control.Exception
+import Control.Monad
 
-type Score = IORef (Int, Int)
+type Score = IORef (Double, Double)
 
 runTests :: [Score -> TestTree] -> IO ()
 runTests groups = do
@@ -24,26 +29,124 @@ runTests groups = do
 tests :: Score -> [Score -> TestTree] -> TestTree
 tests x gs = testGroup "Tests" [ g x | g <- gs ]
 
---------------------------------------------------------------------------------
--- | Construct a single test case
---------------------------------------------------------------------------------
-mkTest' :: (Show b, Eq b) => Score -> (a -> b) -> a -> b -> String -> TestTree
+data TestType = PassTest | FailTest
+
+data TestVector a b = TestVector 
+  { tv_testfn   :: (a -> b) 
+  , tv_testType :: TestType
+  , tv_input    :: a 
+  , tv_expected :: b 
+  , tv_points   :: Double
+  , tv_name     :: String
+  }
+
+data TestCandidate a b = TestCandidate
+  { tc_name     :: String
+  , tc_golden   :: (a -> b) 
+  , tc_flawed   :: (a -> b) 
+  , tc_testType :: TestType
+  , tc_input    :: a
+  , tc_expected :: b
+  }
+
+flipType :: TestType -> TestType 
+flipType t = case t of
+  PassTest -> FailTest
+  FailTest -> PassTest
+
+testTestCandidate :: forall a b. (Show b, Eq b) => String -> TestCandidate a b -> [TestVector a b] -> Double -> Score -> TestTree
+testTestCandidate name TestCandidate{..} currentTests points sc = testGroup name
+    [ testAll name (tests :: [TestVector a b]) points sc ]
+    where
+        replaceFn newFn xs = map (\TestVector{tv_testfn=oldFn,..} -> TestVector{tv_testfn=newFn, ..}) xs
+        -- add tests to ensure
+        -- 1. candFlawed fails the test candidate
+        -- 2. candGolden passes the test candidate
+        -- 3. candFlawed and candGolden pass the existing tests
+        tests    :: [TestVector a b]
+        tests      =  (TestVector tc_flawed (flipType tc_testType) tc_input tc_expected 0 "Flawed fail test")
+                   :  (TestVector tc_flawed (flipType tc_testType) tc_input tc_expected 0 "Golden pass test")
+                   :  (replaceFn tc_flawed currentTests)
+                   ++ (replaceFn tc_golden currentTests)
 
 --------------------------------------------------------------------------------
-scoreTest' :: (Show b, Eq b) => Score -> (a -> b, a, b, Int, String) -> TestTree
+-- | Test and score each test in listTests, optionally scaling to maxPts
 --------------------------------------------------------------------------------
-scoreTest' sc (f, x, expR, points, name) =
+-- tally local score for this list and allow proportional scoring
+-- eg 10pts total, 3/4 pass = 7.5 score.
+testEach :: (Show b, Eq b) => String -> Maybe Double -> [TestVector a b] -> Score -> TestTree
+testEach testName maxPts tvs sc = 
+  withResource 
+    initScore         -- runs before tests
+    (\score_ref -> do -- runs after tests
+      (correct, total) <- readIORef score_ref
+      -- scale score to maxPts or just add to sc
+      let pts = maybe correct (\max -> (correct/total)*max) maxPts
+      updateTotal sc $ fromMaybe total maxPts 
+      updateCurrent sc $ pts
+    )
+    (\group_score ->  -- runs the tests
+      testGroup testName $ map 
+        (\tv@TestVector{..} -> 
+           testCase tv_name $ do
+             score_ref <- group_score
+             mkTest score_ref tv)
+        tvs
+    )
+  
+--------------------------------------------------------------------------------
+-- | Test and score all tests in listTests as one combined test.
+--------------------------------------------------------------------------------
+testAll :: (Show b, Eq b) => String -> [TestVector a b] -> Double -> Score -> TestTree
+testAll name listTests points sc = scoreAllTests sc (listTests, points, name)
+
+--------------------------------------------------------------------------------
+-- | Construct a single compiler test case from a `Program`
+--------------------------------------------------------------------------------
+mkTest :: (Show b, Eq b) => Score -> TestVector a b -> IO ()
+--------------------------------------------------------------------------------
+mkTest sc vector = scoreTest sc vector
+
+--------------------------------------------------------------------------------
+scoreTest :: (Show b, Eq b) => Score -> TestVector a b -> IO ()
+--------------------------------------------------------------------------------
+scoreTest sc TestVector{..} = do
+    updateTotal sc tv_points
+    case tv_testType of
+      PassTest -> if tv_testfn tv_input == tv_expected
+                then updateCurrent sc tv_points
+                else assertFailure "Wrong Result"
+      FailTest -> if tv_testfn tv_input /= tv_expected
+                then updateCurrent sc tv_points
+                else assertFailure "Succeeded Unexpectedly"
+
+scoreAllTests :: (Show b, Eq b) => Score -> ([TestVector a b], Double, String) -> TestTree
+scoreAllTests sc (tests, points, name) = 
   testCase name $ do
     updateTotal sc points
-    if f x == expR
+    groupsc <- initScore
+    forM_ tests (\TestVector{..} -> do
+      updateTotal groupsc points
+      case tv_testType of
+        PassTest -> if tv_testfn tv_input == tv_expected
+                  then updateCurrent groupsc tv_points
+                  else return ()
+        FailTest -> if tv_testfn tv_input /= tv_expected
+                  then updateCurrent groupsc points
+                  else return ())
+    (correct, total) <- readIORef groupsc
+    if correct == total
       then updateCurrent sc points
-      else assertFailure "Wrong Result"
+      else assertFailure "Not all tests passed"
 
-updateTotal :: Score -> Int -> IO ()
+flattenTestGroup :: [[TestTree]] -> [TestTree]
+flattenTestGroup g = foldr (++) [] g
+
+updateTotal :: Score -> Double -> IO ()
 updateTotal sc n = modifyIORef sc (\(x, y) -> (x, y + n))
 
-updateCurrent :: Score -> Int -> IO ()
+updateCurrent :: Score -> Double -> IO ()
 updateCurrent sc n = modifyIORef sc (\(x, y) -> (x + n, y))
 
 initScore :: IO Score
-initScore = newIORef (0, 0)
+initScore = newIORef (0.0, 0.0)
--- tests/Test.hs
+++ tests/Test.hs
@@ -1,93 +1,94 @@
-import Test.Tasty
+{-# LANGUAGE RankNTypes #-}
 import Common
 import Hw1 
+import Hw1B
+import Test.Tasty
+import Common
+import Data.List
 
 main :: IO ()
-main = runTests [unit1]
+main = runTests [ sumListGroup
+                , digitsOfIntGroup
+                , digitsGroup
+                , additivePersistenceGroup
+                , digitalRootGroup
+                , reverseGroup
+                , palindromeGroup
+                ]
+-- Points for each group are defined in README and should be 
+-- kept in sync. Points for individual tests indicate their relative 
+-- value to other tests in the group. The total number of passing 
+-- tests divided by the total possible (individual) points is used 
+-- to scale the group points awarded.
+sumListGroup :: Score -> TestTree
+sumListGroup score_ref = testGroup "sumList Tests" 
+  [ testEach "sumList" (Just 10) (sumListTests sumList) score_ref ]
 
-unit1 :: Score -> TestTree
-unit1 sc = testGroup "Unit 1"
-  [ mkTest
-      sumList
-      []
-      0
-      "sumList 1"
-  ,mkTest
-      sumList
-      [1, 2, 3, 4]
-      10
-      "sumList 2"
-  , mkTest
-      sumList
-      [1, -2, 3, 5]
-      7
-      "sumList 3"
-  , mkTest
-      sumList
-      [1, 3, 5, 7, 9, 11]
-      36
-      "sumList 4"
-  , mkTest
-      digitsOfInt
-      3124
-      [3, 1, 2, 4]
-      "digitsOfInt 1"
-  , mkTest
-      digitsOfInt
-      352663
-      [3, 5, 2, 6, 6, 3]
-      "digitsOfInt 2"
-  , mkTest
-      digits
-      31243
-      [3, 1, 2, 4, 3]
-      "digits 1"
-  , mkTest
-      digits
-      (-23422)
-      [2, 3, 4, 2, 2]
-      "digits 2"
-  , mkTest
-      additivePersistence
-      9876
-      2
-      "additivePersistence 1"
-  , mkTest
-      additivePersistence
-      1
-      0
-      "additivePersistence 2"
-  , mkTest
-      digitalRoot
-      9876
-      3
-      "digitalRoot 1"
-  , mkTest
-      digitalRoot
-      1
-      1
-      "digitalRoot 2"
-  , mkTest
-      listReverse
-      [1, 2, 3, 4]
-      [4, 3, 2, 1]
-      "reverse 1"
-  , mkTest
-      listReverse
-      ["a", "b", "c", "d"]
-      ["d", "c", "b", "a"]
-      "reverse 2"
-  , mkTest
-      palindrome
-      "malayalam"
-      True
-      "palindrome 1"
-  , mkTest
-      palindrome
-      "myxomatosis"
-      False
-      "palindrome 2"
+sumListTests :: ([Int] -> Int) -> [TestVector [Int] Int]
+sumListTests testfn =
+  [ TestVector testfn PassTest [] 0                   1 "sumList 1"
+  , TestVector testfn PassTest [1, 2, 3, 4] 10        1 "sumList 2"
+  , TestVector testfn PassTest [1, -2, 3, 5] 7        1 "sumList 3"
+  , TestVector testfn FailTest [1, 3, 5, 7, 9, 11] 35 1 "sumList 4"
   ]
-  where
-    mkTest :: (Show b, Eq b) => (a -> b) -> a -> b -> String -> TestTree
-    mkTest = mkTest' sc
+
+digitsOfIntGroup :: Score -> TestTree
+digitsOfIntGroup score_ref = testGroup "digitsOfInt Tests" 
+  [ testEach "digitsOfInt" (Just 8) (digitsOfIntTests digitsOfInt) score_ref ]
+
+digitsOfIntTests :: (Int -> [Int]) -> [TestVector Int [Int]]
+digitsOfIntTests testfn = 
+  [ TestVector testfn PassTest 3124 [3,1,2,4]            1 "digitsOfInt 1"
+  , TestVector testfn PassTest 352663 [3, 5, 2, 6, 6, 3] 1 "digitsOfInt 2"
+  ]
+  
+digitsGroup :: Score -> TestTree
+digitsGroup score_ref = testGroup "digits Tests" 
+  [ testEach "digits" (Just 2) (digitsTests digits) score_ref ]
+
+digitsTests :: (Int -> [Int]) -> [TestVector Int [Int]]
+digitsTests testfn = 
+  [ TestVector testfn PassTest 31243 [3, 1, 2, 4, 3]    1 "digits 1"
+  , TestVector testfn PassTest (-23422) [2, 3, 4, 2, 2] 1 "digits 2"
+  ]
+
+additivePersistenceGroup :: Score -> TestTree
+additivePersistenceGroup score_ref = testGroup "additivePersistence Tests" 
+  [ testEach "additivePersistence" (Just 10) (additivePersistenceTests additivePersistence) score_ref ]
+
+additivePersistenceTests :: (Int -> Int) -> [TestVector Int Int]
+additivePersistenceTests testfn = 
+  [ TestVector testfn PassTest 9876 2 1 "additivePersistence 1"
+  , TestVector testfn PassTest 1 0    1 "additivePersistence 2"
+  ]
+
+digitalRootGroup :: Score -> TestTree
+digitalRootGroup score_ref = testGroup "digitalRoot Tests" 
+  [ testEach "digitalRoot" (Just 10) (digitalRootTests digitalRoot) score_ref ]
+
+digitalRootTests :: (Int -> Int) -> [TestVector Int Int]
+digitalRootTests testfn = 
+  [ TestVector testfn PassTest 9876 3 1 "digitalRoot 1"
+  , TestVector testfn PassTest 1 1    1 "digitalRoot 2"
+  ]
+
+reverseGroup :: Score -> TestTree
+reverseGroup score_ref = testGroup "reverse Tests" 
+ ( [ testEach "reverseInt" (Just 7.5) (reverseIntTests reverse) score_ref ]
+  ++ [ testEach "reverseStr" (Just 7.5) (reverseStrTests reverse) score_ref ])
+
+reverseIntTests :: ([Int] -> [Int]) -> [TestVector [Int] [Int]]
+reverseIntTests testfn = [ TestVector testfn PassTest [1, 2, 3, 4] [4, 3, 2, 1] 1 "reverse 1" ]
+
+reverseStrTests :: ([String] -> [String]) -> [TestVector [String] [String]]
+reverseStrTests testfn = [ TestVector testfn PassTest ["a", "b", "c", "d"] ["d", "c", "b", "a"] 1 "reverse 2" ]
+
+palindromeGroup :: Score -> TestTree
+palindromeGroup score_ref = testGroup "palindrome Tests" 
+  [ testEach "palindrome" (Just 10) (palindromeTests palindrome) score_ref ]
+
+palindromeTests :: (String -> Bool) -> [TestVector String Bool]
+palindromeTests testfn = 
+  [ TestVector testfn PassTest "malayalam" True    1 "palindrome 1"
+  , TestVector testfn PassTest "myxomatosis" False 1 "palindrome 2"
+  ]
